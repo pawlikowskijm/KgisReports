9c706dee8b54b6fa02e80421751fca3bb4fbbbac
Michał Pawlikowski <michalpw159@gmail.com>
2018-11-24 12:39:12 +0100

    Add project files.

---
+++ KgisReports/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace KgisReports
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ KgisReports/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="KgisReports.MvcApplication" Language="C#" %>
